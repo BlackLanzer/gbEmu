89f668f3fc618bea64552c55bd56c52a8334b669
BlackLanzer <ale.sensei@yahoo.it>
2015-10-13 15:51:41 +0200

    Instructions 0x37-0x7F

--- gbEmu/instructions.h
+++ gbEmu/instructions.h
@@ -454,12 +454,12 @@ case 0x1F:
 // 0x20 JR NZ, n
 case 0x20:
 {
-	regs.pc++;
 	if (regs.f & 0x80 == 0)
 	{
 		// jump
 		regs.pc += mmu.readByte(regs.pc);
 	}
+	regs.pc++;
 	clock += 2;
 	// instructionName = "JR NZ, " + n;
 	break;
@@ -612,12 +612,12 @@ case 0x27:
 // 0x28 JR Z, n
 case 0x28:
 {
-	regs.pc++;
 	if (regs.f & 0x80 == 1)
 	{
 		// jump
 		regs.pc += mmu.readByte(regs.pc);
 	}
+	regs.pc++;
 	clock += 2;
 	// instructionName = "JR Z, " + n;
 	break;
@@ -651,13 +651,13 @@ case 0x29:
 	//instructionName = "ADD HL, HL";
 	break;
 }
-// 0x2A LD HL, (nn)
+// 0x2A LDI A,(HL)
 case 0x2A:
 {
-	regs.hl = mmu.readWord(mmu.readWord(regs.pc));
-	regs.pc += 2;
-	clock += 5;
-	// instructionName = "LD HL, " + nn;
+	regs.a = mmu.readByte(regs.hl);
+	regs.hl++;
+	clock += 2;
+	// instructionName = "LDI A, (HL)";
 	break;
 }
 // 0x2B DEC HL
@@ -715,7 +715,7 @@ case 0x2D:
 	//instructionName = "DEC L";
 	break;
 }
-// 0x2E LD L,n
+// 0x2E LD L, n
 case 0x2E:
 {
 	regs.l = mmu.readByte(regs.pc++);
@@ -737,11 +737,11 @@ case 0x2F:
 // 0x30 JR NC, n
 case 0x30:
 {
-	regs.pc++;
 	if (regs.f & 0x10 == 0)
 	{	// jump
 		regs.pc += mmu.readByte(regs.pc);
 	}
+	regs.pc++;
 	clock += 2;
 	// instructionName = "JR NC, " + n;
 	break;
@@ -831,6 +831,637 @@ case 0x36:
 	// instructionName = "LD (HL), " + n;
 	break;
 }
+// 0x37 SCF
+case 0x37:
+{	// Set Carry Flag
+	// flags: -001
+	flags = flags & ~0x40 & ~0x20 | 0x10;
+	clock++;
+	// instructionName = "SCF";
+	break;
+}
+// 0x38 JR C, n
+case 0x38:
+{
+	if (regs.f & 0x10 == 1)
+	{	// jump
+		regs.pc += mmu.readByte(regs.pc);
+	}
+	regs.pc++;
+	clock += 2;
+	// instructionName = "JR C, " + n;
+	break;
+}
+// 0x39 ADD HL, SP
+case 0x39:
+{
+	// half carry flag
+	// non ho idea di come funziona
+	byte hFlag = (((regs.hl & 0x0FFF) + (regs.sp & 0x0FFF)) > 0x0FFF) ? 1 : 0;
+    if (hFlag == 0)
+    	regs.f &= ~0x20;
+    else
+    	regs.f |= 0x20; 
+	int tmp = regs.hl + regs.sp;
+	regs.hl = tmp;
+	// flags: -0hc----
+	// add/sub flag a 0
+	regs.f &= 0xBF; // 0xBF = 10111111  
+	// carry flag
+	if (tmp > 0xFFFF)
+	{	// set 1
+		regs.f |= 0x10; // 0x10 = 00010000
+	} 
+	else
+	{	// set 0
+		regs.f &= 0xEF; // 0xEF = 11101111
+	}
+	clock += 2;
+	//instructionName = "ADD HL, SP";
+	break;
+}
+// 0x3A LDD A, (HL)
+case 0x3A:
+{
+	regs.a = mmu.readByte(regs.hl);
+	regs.hl--;
+	clock += 2;
+	// instructionName = "LDD A, (HL)";
+	break;
+}
+// 0x3B DEC SP
+case 0x3B:
+{
+	regs.SP--;
+	clock += 2;
+	//instructionName = "DEC SP";
+	break;	
+}
+// 0x3C INC A
+case 0x3C:
+{
+	regs.a++;
+	// flags
+	// zero flag
+	if (regs.a == 0)
+		regs.f |= 0x80;
+	else
+		regs.f &= 0x7F;
+	// add/sub flag a 0
+	regs.f &= 0xBF;
+	// half carry flag
+	// non ho idea di come funziona
+    byte hFlag = (regs.a & 0x0F ? 0 : 1);
+    if (hFlag == 0)
+    	regs.f &= ~0x20;
+    else
+    	regs.f |= 0x20; 
+
+	clock++;
+	//instructionName = "INC A";
+	break;
+}
+// 0x3D DEC A
+case 0x3D:
+{
+	regs.a--;
+	// flags
+	// zero flag
+	if (regs.a == 0)
+		regs.f |= 0x80;
+	else
+		regs.f &= 0x7F;
+	// add/sub flag
+	regs.f |= 0x40;
+	// half carry flag
+	// non ho idea di come funziona
+    byte hFlag = ((regs.a & 0x0F) == 0x0F) ? 1 : 0);
+    if (hFlag == 0)
+    	regs.f &= ~0x20;
+    else
+    	regs.f |= 0x20; 
+	clock++;
+	//instructionName = "DEC A";
+	break;
+}
+// 0x3E LD A, n
+case 0x3E:
+{
+	regs.a = mmu.readByte(regs.pc++);
+	clock += 2;
+	// instructionName = "LD A, " + n;
+	break;
+}
+// 0x3F CCF
+case 0x3F:
+{	// Complement Carry Flag
+	// flags: -00c
+	regs.f &= ~0x20 & ~0x40 & ~(regs.f & 0x10);
+	clock++; 
+	// instructionName = "CCF";
+	break;
+}
+// 0x40 LD B, B
+case 0x40:
+{
+	clock++;
+	// instructionName = "LD B, B";
+	break;
+}
+// 0x41 LD B, C
+case 0x41:
+{
+	regs.b = regs.c;
+	clock++;
+	// instructionName = "LD B, C";
+	break;
+}
+// 0x42 LD B, D
+case 0x42:
+{
+	regs.b = regs.d;
+	clock++;
+	// instructionName = "LD B, D";
+	break;	
+}
+// 0x43 LD B, E
+case 0x43:
+{
+	regs.b = regs.e;
+	clock++;
+	// instructionName = "LD B, E";
+	break;
+}
+// 0x44 LD B, H
+case 0x44:
+{
+	regs.b = regs.h;
+	clock++;
+	// instructionName = "LD B, H";
+	break;
+}
+// 0x45 LD B, L
+case 0x45:
+{
+	regs.b = regs.l;
+	clock++;
+	// instructionName = "LD B, L";
+	break;
+}
+// 0x46 LD B, (HL)
+case 0x46:
+{
+	regs.b = mmu.readByte(regs.hl);
+	clock += 2;
+	// instructionName = "LD B, (HL)";
+}
+// 0x47 LD B, A
+case 0x47:
+{
+	regs.b = regs.a;
+	clock++;
+	// instructioName = "LD B, C";
+	break;
+}
+// 0x48 LD C, B
+case 0x48:
+{
+	regs.c = regs.b;
+	clock++;
+	// instructionName = "LD C, B";
+	break;
+}
+// 0x49 LD C, C
+case 0x49:
+{
+	clock++;
+	// instructionName = "LD C, C";
+}
+// 0x4A LD C, D
+case 0x4A:
+{
+	regs.c = regs.d;
+	clock++;
+	// instructionName = "LD C, D";
+	break;
+}
+// 0x4B LD C, E
+case 0x4B:
+{
+	regs.c = regs.e;
+	clock++;
+	// instructionName = "LD C, E";
+	break;
+}
+// 0x4C LD C, H
+case 0x4C:
+{
+	regs.c = regs.h;
+	clock++;
+	// instructionName = "LD C, H";
+	break;
+}
+// 0x4D LD C, L
+case 0x4D:
+{
+	regs.c = regs.l;
+	clock++;
+	// instructionName = "LD C, L";
+	break;
+}
+// 0x4E LD C, (HL)
+case 0x4E:
+{
+	regs.c = mmu.readByte(regs.hl);
+	clock += 2;
+	instructionName = "LD C, (HL)";
+	break;
+}
+// 0x4F LD C, A
+case 0x4F:
+{
+	regs.c = regs.a;
+	clock++;
+	// instructionName = "LD C, A";
+	break;
+}
+// 0x50 LD D, B
+case 0x50:
+{
+	regs.d = regs.b;
+	clock++;
+	// instructionName = "LD D, B";
+	break;
+}
+// 0x51 LD D, C
+case 0x51:
+{
+	regs.d = regs.c;
+	clock++;
+	// instructionName = "LD D, C";
+	break;
+}
+// 0x52 LD D, D
+case 0x52:
+{
+	regs.d = regs.d;
+	clock++;
+	// instructionName = "LD D, D";
+	break;	
+}
+// 0x53 LD D, E
+case 0x53:
+{
+	regs.d = regs.e;
+	clock++;
+	// instructionName = "LD D, E";
+	break;
+}
+// 0x54 LD D, H
+case 0x54:
+{
+	regs.d = regs.h;
+	clock++;
+	// instructionName = "LD D, H";
+	break;
+}
+// 0x55 LD D, L
+case 0x55:
+{
+	regs.d = regs.l;
+	clock++;
+	// instructionName = "LD D, L";
+	break;
+}
+// 0x56 LD D, (HL)
+case 0x56:
+{
+	regs.d = mmu.readByte(regs.hl);
+	clock += 2;
+	// instructionName = "LD D, (HL)";
+}
+// 0x57 LD D, A
+case 0x57:
+{
+	regs.d = regs.a;
+	clock++;
+	// instructioName = "LD D, C";
+	break;
+}
+// 0x58 LD E, B
+case 0x58:
+{
+	regs.e = regs.b;
+	clock++;
+	// instructionName = "LD E, B";
+	break;
+}
+// 0x59 LD E, C
+case 0x59:
+{
+	clock++;
+	// instructionName = "LD E, C";
+}
+// 0x5A LD E, D
+case 0x5A:
+{
+	regs.e = regs.d;
+	clock++;
+	// instructionName = "LD E, D";
+	break;
+}
+// 0x5B LD E, E
+case 0x5B:
+{
+	regs.e = regs.e;
+	clock++;
+	// instructionName = "LD E, E";
+	break;
+}
+// 0x5C LD E, H
+case 0x5C:
+{
+	regs.e = regs.h;
+	clock++;
+	// instructionName = "LD E, H";
+	break;
+}
+// 0x5D LD E, L
+case 0x5D:
+{
+	regs.e = regs.l;
+	clock++;
+	// instructionName = "LD E, L";
+	break;
+}
+// 0x5E LD E, (HL)
+case 0x5E:
+{
+	regs.e = mmu.readByte(regs.hl);
+	clock += 2;
+	instructionName = "LD E, (HL)";
+	break;
+}
+// 0x5F LD E, A
+case 0x5F:
+{
+	regs.e = regs.a;
+	clock++;
+	// instructionName = "LD E, A";
+	break;
+}
+// 0x60 LD H, B
+case 0x60:
+{
+	regs.h = regs.b;
+	clock++;
+	// instructionName = "LD H, B";
+	break;
+}
+// 0x61 LD H, C
+case 0x61:
+{
+	regs.h = regs.c;
+	clock++;
+	// instructionName = "LD H, C";
+	break;
+}
+// 0x62 LD H, D
+case 0x62:
+{
+	regs.h = regs.d;
+	clock++;
+	// instructionName = "LD H, D";
+	break;	
+}
+// 0x63 LD H, E
+case 0x63:
+{
+	regs.h = regs.e;
+	clock++;
+	// instructionName = "LD H, E";
+	break;
+}
+// 0x64 LD H, H
+case 0x64:
+{
+	regs.h = regs.h;
+	clock++;
+	// instructionName = "LD H, H";
+	break;
+}
+// 0x65 LD H, L
+case 0x65:
+{
+	regs.h = regs.l;
+	clock++;
+	// instructionName = "LD H, L";
+	break;
+}
+// 0x66 LD H, (HL)
+case 0x66:
+{
+	regs.h = mmu.readByte(regs.hl);
+	clock += 2;
+	// instructionName = "LD H, (HL)";
+}
+// 0x67 LD H, A
+case 0x67:
+{
+	regs.h = regs.a;
+	clock++;
+	// instructioName = "LD H, C";
+	break;
+}
+// 0x68 LD L, B
+case 0x68:
+{
+	regs.l = regs.b;
+	clock++;
+	// instructionName = "LD L, B";
+	break;
+}
+// 0x69 LD L, C
+case 0x69:
+{
+	clock++;
+	// instructionName = "LD L, C";
+}
+// 0x6A LD L, D
+case 0x6A:
+{
+	regs.l = regs.d;
+	clock++;
+	// instructionName = "LD L, D";
+	break;
+}
+// 0x6B LD L, E
+case 0x6B:
+{
+	regs.l = regs.e;
+	clock++;
+	// instructionName = "LD L, E";
+	break;
+}
+// 0x6C LD L, H
+case 0x6C:
+{
+	regs.l = regs.h;
+	clock++;
+	// instructionName = "LD L, H";
+	break;
+}
+// 0x6D LD L, L
+case 0x6D:
+{
+	regs.l = regs.l;
+	clock++;
+	// instructionName = "LD L, L";
+	break;
+}
+// 0x6E LD L, (HL)
+case 0x6E:
+{
+	regs.l = mmu.readByte(regs.hl);
+	clock += 2;
+	instructionName = "LD L, (HL)";
+	break;
+}
+// 0x6F LD L, A
+case 0x6F:
+{
+	regs.l = regs.a;
+	clock++;
+	// instructionName = "LD L, A";
+	break;
+}
+// 0x70 LD (HL), B
+case 0x70:
+{
+	mmu.writeByte(regs.hl, regs.b);
+	clock += 2;
+	// instructionName = "LD (HL), B";
+	break;
+}
+// 0x71 LD (HL), C
+case 0x71:
+{
+	mmu.writeByte(regs.hl, regs.c);
+	clock += 2;
+	// instructionName = "LD (HL), C";
+	break;
+}
+// 0x72 LD (HL), D
+case 0x72:
+{
+	mmu.writeByte(regs.hl, regs.d);
+	clock += 2;
+	// instructionName = "LD (HL), D";
+	break;
+}
+// 0x73 LD (HL), E
+case 0x73:
+{
+	mmu.writeByte(regs.hl, regs.e);
+	clock += 2;
+	// instructionName = "LD (HL), E";
+	break;
+}
+// 0x74 LD (HL), H
+case 0x74:
+{
+	mmu.writeByte(regs.hl, regs.h);
+	clock += 2;
+	// instructionName = "LD (HL), H";
+	break;
+}
+// 0x75 LD (HL), L
+case 0x75:
+{
+	mmu.writeByte(regs.hl, regs.l);
+	clock += 2;
+	// instructionName = "LD (HL), L";
+	break;
+}
+// 0x76 HALT
+case 0x76:
+{
+	// non serve a niente
+	clock++;
+	// instructionName = "HALT";
+	break;
+}
+// 0x77 LD (HL), A
+case 0x77:
+{
+	mmu.writeByte(regs.hl, regs.a);
+	clock += 2;
+	// instructionName = "LD (HL), A";
+	break;
+}
+// 0x78 LD A, B
+case 0x78:
+{
+	regs.a = regs.b;
+	clock++;
+	// instructionName = "LD A, B";
+	break;
+}
+// 0x79 LD A, C
+case 0x79:
+{
+	clock++;
+	// instructionName = "LD A, C";
+}
+// 0x7A LD A, D
+case 0x7A:
+{
+	regs.a = regs.d;
+	clock++;
+	// instructionName = "LD A, D";
+	break;
+}
+// 0x7B LD A, E
+case 0x7B:
+{
+	regs.a = regs.e;
+	clock++;
+	// instructionName = "LD A, E";
+	break;
+}
+// 0x7C LD A, H
+case 0x7C:
+{
+	regs.a = regs.h;
+	clock++;
+	// instructionName = "LD A, H";
+	break;
+}
+// 0x7D LD A, L
+case 0x7D:
+{
+	regs.a = regs.l;
+	clock++;
+	// instructionName = "LD A, L";
+	break;
+}
+// 0x7E LD A, (HL)
+case 0x7E:
+{
+	regs.a = mmu.readByte(regs.hl);
+	clock += 2;
+	instructionName = "LD A, (HL)";
+	break;
+}
+// 0x7F LD A, A
+case 0x7F:
+{
+	regs.a = regs.a;
+	clock++;
+	// instructionName = "LD A, A";
+	break;
+}
+
 
 
 // 0xC3 JP nn
